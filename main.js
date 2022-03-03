4c0a873e31bebf3c5e770d5cd5cf443dee8e5f2d
r_code_explore <rciupercher@gmail.com>
2022-03-03 15:35:51 +0100

    Js: done! css: done! project: done!

--- main.js
+++ main.js
@@ -1,31 +1,104 @@
-let bordScore;
-let scores;
-let activePlayer;
-let activePlaying;
+//Rappel des règles dans la page (Readme)
+// Game rules in readme page
 
-init();
+//def variables
 
-//function d'initialisation des variables
-function init() {
-    //Partie 1: reinitialisation des variables
-    bordScore = 0;
-    scores = [0, 0];
-    activePlayer = 0;
-    activePlaying = true; 
+let playing, scoreBoard, scores, activePlayer;
 
-    //Actualisation des points visibles
-    document.getElementById('nom_1').innerText = 'Joueur 1';
-    document.getElementById('nom_2').innerText = 'Joueur 2';
-    document.getElementById('actuel_1').innerText = '0';
-    document.getElementById('actuel_2').innerText = '0';
-    document.getElementById('score_1').innerText = '0';
-    document.getElementById('score_2').innerText = '0';
-    document.querySelector('.de').style.display = 'none';
+init()
 
-    //reinitialisation des classes
-    document.querySelector('.panel_joueur_1').classList.remove('active');
-    document.querySelector('.panel_joueur_1').classList.remove('win');
-    document.querySelector('.panel_joueur_2').classList.remove('active');
-    document.querySelector('.panel_joueur_2').classList.remove('win');
-    document.querySelector('.panel_joueur_1').classList.add('active');
+function init(){
+    //Reinitialisation variables
+    playing = true
+    scoreBoard = 0
+    scores = [0, 0]
+    activePlayer = 0
+
+    // Mise à jour scores
+    document.getElementById('name-0').innerText = 'Joueur 1'
+    document.getElementById('name-1').innerText = 'Joueur 2'
+    document.getElementById('actual-0').innerText = '0'
+    document.getElementById('actual-1').innerText = '0'
+    document.getElementById('score-0').innerText = '0'
+    document.getElementById('score-1').innerText = '0'
+
+    //Reinitialisation image du dé
+    document.querySelector('.dice').style.display = 'none'
+
+    //Reinitialisation classes
+
+    document.querySelector('.player-0-panel').classList.remove('active')
+    document.querySelector('.player-0-panel').classList.remove('winner')
+    document.querySelector('.player-1-panel').classList.remove('active')
+    document.querySelector('.player-1-panel').classList.remove('winner')
+    document.querySelector('.player-0-panel').classList.add('active')
 }
+
+//fonction de changement de joueur
+function changePlayer(){
+    //Remise à 0 du score courant
+    scoreBoard = 0
+
+    document.getElementById('actual-' + activePlayer).innerText = scoreBoard
+
+    //réinitialisation joueur actif
+    activePlayer == 0 ? activePlayer = 1 : activePlayer = 0
+
+    //blockage du dé
+    document.querySelector('.dice').style.display = 'none'
+    //
+
+    document.querySelector('.player-0-panel').classList.remove('active')
+    document.querySelector('.player-1-panel').classList.remove('active')
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('active')
+}
+
+//Roulement Dé
+document.querySelector('.btn-roll').addEventListener('click', () =>{
+    if(playing) {
+
+    let dice = Math.floor(Math.random() * 6 + 1)
+
+    //Si ce n'est pas = 1
+    if(dice != 1){
+        scoreBoard += dice
+
+        document.getElementById('actual-' + activePlayer).innerText = scoreBoard;
+        document.querySelector('.dice').style.display = 'block'
+        document.querySelector('.dice').src = 'assets/dice-' + dice + '.png'
+    } else {
+        //changement de joueur
+
+        changePlayer();
+    }
+}
+})
+
+//Passage a la fonction HOLD permettant de rassembler les points accumulés dans la partie "round global"
+
+document.querySelector('.btn-hold').addEventListener('click', () => {
+    if(playing){
+    scores[activePlayer] += scoreBoard;
+    document.getElementById('score-' + activePlayer).innerText = scores[activePlayer];
+    //Si le joueur actif arrive à 100 ou plus, il gagne
+    if (scores[activePlayer] >= 100) {
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+
+        //Affichage du message du gagnat
+        document.getElementById('name-' + activePlayer).innerText = 'Gagnant !'
+        //arrêt de partie
+        playing = false
+
+        alert('Appuyer sur "New Game" pour recommencer')
+    } else {
+        //Sinon changement de joueur
+        changePlayer();
+    }
+}
+})
+
+//Partie de new game / Relancer une nouvelle partie
+
+document.querySelector('.btn-new').addEventListener('click', () => {
+    init()
+})
